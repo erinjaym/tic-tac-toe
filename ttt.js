437c9864746f53d1426f9cfce1f5e84500eea8aa
EJM <erin.jay.moyer@gmail.com>
2021-03-22 23:27:50 -0600

    Finished initial display functionality. Added tracking to gameMat location. Created event listener to pass player choice.

--- ttt.js
+++ ttt.js
@@ -4,64 +4,70 @@
 //module attempt
 const gameMat = (function () 
 {
-    //var _privateThings = "private";
 
     let theGameMat = new Array(9);
     const initialize = () => theGameMat.fill("empty", 0, 9);
+    const playerSelection = (playersWeapon, matLocation) =>  { 
+        if (theGameMat[matLocation] == "X" || theGameMat[matLocation] == "O" )
+        {
+            alert("That Spot is taken");
+        }
 
+        else 
+        {
+            theGameMat[matLocation] = playersWeapon;
+        }
+    }
 
-  //  const playerSpotX = "X";
-   // const playerSpotY = "Y";
-   // const emptySpace = "empty";
+    const playerWeapon = (weapon) => player.weapon = weapon; // this to player factory?
 
    const display = () =>
     {
-        for(rows = 0; rows <= 3; rows++ )
+        let gameMatTracker = 0; // keep track of game mat location may need to alter later
+        for(rows = 0; rows <= 2; rows++ )
         {
-            const square = document.createElement('div');
-            square.className = "square";
+            
             //add marker to array as empty? 
-           // make each row;
-            //draw shit on screen;
 
-            for (columns = 0; columns <=3; columns++)
+            for (columns = 0; columns <= 2; columns++)
             {
-                //make each column in each row;
-                //draw shit on screen;
+                const square = document.createElement('div');
+                square.className = "square";
+                square.setAttribute("id", [gameMatTracker]); // sets ID to match location on game mat
+                square.textContent = theGameMat[gameMatTracker]; // add text from game mat at location matching
+                gameMatTracker += 1; // newRow.setAttribute("id", [bookLocation]);   from bookLibrary?
+            
+                const container = document.getElementById("container");
+                container.appendChild(square);
+
             }
         }
     }
 
-  //  function _privateMethod () 
-  //  {
-  //      console.log("private");
-  //  }
 
-
- //   return 
-  //  { somethingPublic: function ()
-  //      {console.log("someshit for the world!");}
-  //  }
   return { initialize, display,}; // add gameMat if you want to check array is correct // these Items are public
 
 }) ();
-
-
 
 gameMat.initialize(); // makes a blank board on start 
 gameMat.display(); // makes initial display on screen
 
 
 
-
-
-
-
-// function factory 
 const playerFactory = (selection) => 
 {
-    let playerSelection = selection;
+    //const name = playerName;
+  //  const playerWeaponChoice =>    // x or 0 selection 
+   // let playerSelection = selection;
 
     const saySayonara = () => console.log("Kiss your ass goodbye");
-    return {name, saySayonara};
+    return {saySayonara};
 }
+
+
+document.getElementById('container').addEventListener("click", function (e)
+{
+let matLocation = e.target.id;
+console.log(matLocation);
+// send matLocation to a function that adds to the mat  and which player?? ..> playerSelection(matLocation);
+});
