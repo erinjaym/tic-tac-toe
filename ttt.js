66f61d4efce9b89ce33abaa9cf30505cc6775357
EJM <erin.jay.moyer@gmail.com>
2021-03-30 14:37:07 -0600

    Tie game no longer shows chracter turn images. Replay button now populates after a tie. Cleaned and removed testing code from javascript file.

--- ttt.js
+++ ttt.js
@@ -1,7 +1,3 @@
-
-
-
-//module attempt
 const gameMat = (function () 
 {
     let lastPlayer = "placeholder"
@@ -11,32 +7,23 @@ const gameMat = (function ()
         if (theGameMat[matLocation] == "X" || theGameMat[matLocation] == "O" )
         {
             alert("That Spot is taken");
-            return false; // tell gameController that not succesfull
+            return false; 
         }
 
         else 
         {
             lastPlayer = playersName;
-            // remove existing element from display?
             const square = document.getElementById(matLocation);
             square.textContent = playersWeapon;
-            //testing graphics
-            //const theX = document.createElement('div');
-            //theX.className = "O";
-           // square.appendChild(theX);
-            // square.appendChild(); for visual objects later
-            // put in array to log fill
             theGameMat[matLocation] = playersWeapon;
-
-            console.log(winnerCheck());
-            tieCheck();
+            tieCheck(); // checks for ties and winners
             return true;
         }
     }
-//Glitch in display ... if manage to click parent container will crash
+    
    const display = () =>
     {
-        let gameMatTracker = 0; // keep track of game mat location may need to alter later
+        let gameMatTracker = 0; 
         for(rows = 0; rows <= 2; rows++ )
         {
             
@@ -45,20 +32,16 @@ const gameMat = (function ()
                 const square = document.createElement('div');
                 square.className = "square";
                 square.setAttribute("id", [gameMatTracker]); // sets ID to match location on game mat
-                
-                //square.textContent = theGameMat[gameMatTracker]; // add text from game mat at location matching
-                gameMatTracker += 1; // newRow.setAttribute("id", [bookLocation]);   from bookLibrary?
+                gameMatTracker += 1; 
                 const container = document.getElementById("container");
                 container.appendChild(square);
-
             }
         }
     }
 
-
-    function winnerCheck () 
-    {  // can improve performance by adding a quick out in inner loop
         // winner check stops game if true!!!!! 
+    function winnerCheck () 
+    { 
         if (horizontalCheck() == true)
             {    
                 revealWinner();
@@ -74,13 +57,16 @@ const gameMat = (function ()
             revealWinner();
             return true;
         }
-        else {return false;}
+        else 
+        {
+            return false;
+        }
 
         function horizontalCheck () {
             for (let startPoint = 0; startPoint <= 6; startPoint+=3) 
             {
                 let SP = startPoint;
-                if (theGameMat[SP] != "empty") // rule out empty
+                if (theGameMat[SP] != "empty")
                 {
                     if (theGameMat[SP] == theGameMat[SP + 1] && theGameMat[SP] == theGameMat[SP + 2])
                      {
@@ -101,7 +87,7 @@ const gameMat = (function ()
             for (let vertPoint = 0; vertPoint <= 2; vertPoint += 1) 
             {
                 let VSP = vertPoint;
-                if (theGameMat[VSP] != "empty") // rule out empty
+                if (theGameMat[VSP] != "empty") 
                 {
                     if (theGameMat[VSP] == theGameMat[VSP + 3] && theGameMat[VSP] == theGameMat[VSP + 6])
                     {
@@ -139,8 +125,8 @@ const gameMat = (function ()
                 if (lastPlayer == "Inu") // reveal dog pic
                 {
                     document.getElementById('winner-pic-crow').remove();    // remove non winners
-                    document.getElementById("winner-notification").style.display = "grid"; //show popup
-                    document.getElementById("replay-button").style.display = "grid";
+                    document.getElementById("winner-notification").style.display = "grid"; //show winner popup
+                    document.getElementById("replay-button").style.display = "grid"; // show replay button
                 }
                 else if (lastPlayer == "Karasu") // reveal crow pic
                 {
@@ -148,38 +134,34 @@ const gameMat = (function ()
                     document.getElementById("winner-notification").style.display = "grid";
                     document.getElementById("replay-button").style.display = "grid";
                 }
-                else // in case more characters later on
+                else 
                 {
-        
                 }
-        
             }
-
 
     } // end of winner check 
 
     function tieCheck () 
     {
-        console.log("tieCheck");
-
-        if (checkForEmpty() == true)
+        if (checkForEmpty() == true)    // Rule out empty spots
         {
             return false;
         }
-        else if (winnerCheck() == true) 
+        else if (winnerCheck() == true)  // Rule out winners 
         {
             return false;
         }
         else // theres a tie woot! 
         {
-            document.getElementById('tie-notification').style.display = "grid";
+            document.getElementById("replay-button").style.display = "grid"; // show replay button
+            document.getElementById('tie-notification').style.display = "grid"; // show tie notification
             return true;
         }
 
         function checkForEmpty(){
         for (spotCheck = 0; spotCheck <= theGameMat.length -1; spotCheck ++)
             {
-                if (theGameMat[spotCheck] == "empty") // game not over found empty spot
+                if (theGameMat[spotCheck] == "empty") 
                 {
                     return true; 
                 }
@@ -192,26 +174,28 @@ const gameMat = (function ()
       
     }
     
-
-
     function playersTurnDisplay (player)
     {
 
-        if (winnerCheck() == true) // bascase // add more players later
+        if (winnerCheck() == true) // hide player turns on win 
         {
             document.getElementById("dog").style.display = "none";
             document.getElementById("crow").style.display = "none";
         }
-
+        else if (tieCheck() == true) // hide players if tie
+        {
+            document.getElementById("dog").style.display = "none";
+            document.getElementById("crow").style.display = "none";
+        }
         else if (player.name == "Inu") 
         {
-            document.getElementById("crow").style.display = "none";// need to fix in the am
+            document.getElementById("crow").style.display = "none";
             document.getElementById("dog").style.display = "grid";
         }
         else if (player.name == "Karasu")     
         {
             document.getElementById("dog").style.display = "none";
-            document.getElementById("crow").style.display = "grid";// need to fix in the am
+            document.getElementById("crow").style.display = "grid";
         } 
         else  // spot for future players and basecase
         {
@@ -222,11 +206,8 @@ const gameMat = (function ()
 
 
 
-  return { initialize, display, playerSelectLocation, winnerCheck, playersTurnDisplay}; // add gameMat if you want to check array is correct // these Items are public
-
-}) ();
-
-
+  return { initialize, display, playerSelectLocation, winnerCheck, playersTurnDisplay}; 
+}) (); // END OF GAMEMAT MODULE
 
 
 
@@ -235,15 +216,10 @@ return { name, weapon};
 };
 
 
-
-
-
-
-
 const gameController = (function ()
 {
-    gameMat.initialize();  // set up initial emtyGameMat
-    gameMat.display();  // set up initial gameboard
+    gameMat.initialize();  
+    gameMat.display();  // setup setup initial gameboard display 
     let playerOne = playerMaker("bob", "X" );
     let playerTwo = playerMaker("bill", "O");
     let playerOneWeapon = "Fists of fury";
@@ -251,18 +227,16 @@ const gameController = (function ()
 
     function createPlayers ()
     {
-
         if(playerOneName == "Not Selected" || playerOneWeapon == "Fists of fury") // check for selection
         {
-            //dont do anything
             alert('Please choose both a character and a weapon to play.');
         }
         else
         {
         playerOne = playerMaker(playerOneName, playerOneWeapon);
         setPlayerTwo();
-        document.getElementById("player-selection-container").style.display = "none";
-        gamePlay().startGame(); // may move to later on in 
+        document.getElementById("player-selection-container").style.display = "none"; // hide selection container
+        gamePlay().startGame();
         return playerOne;
         }
     }
@@ -302,27 +276,21 @@ const gameController = (function ()
         return playerTwo;
     }
 
-
-    // save variable input as weapon
-    // change weapon select class to activate  (disapble hoovering)
-
     function setWeapon (weaponInput)
     {
 
         if (weaponInput == "X" )
         {
 
-            if (document.getElementById("O").className == "player-selected") // if O already selected
+            if (document.getElementById("O").className == "player-selected") // O already selected
             {
-            document.getElementById("O").className = "player-select"
-            console.log("X");
-            document.getElementById("X").className = "player-selected";
-            playerOneWeapon = weaponInput;
-            return true;
+                document.getElementById("O").className = "player-select"    // return o to unselected state
+                document.getElementById("X").className = "player-selected";
+                playerOneWeapon = weaponInput;
+                return true;
             }
             else
             {
-                console.log("X");
                 document.getElementById("X").className = "player-selected";
                 playerOneWeapon = weaponInput;
                 return true;
@@ -333,16 +301,56 @@ const gameController = (function ()
             if (document.getElementById("X").className == "player-selected") //x already selected
             {
                 document.getElementById("X").className = "player-select"; // return x to unselected state
-                console.log("O");
                 document.getElementById("O").className = "player-selected";
                 playerOneWeapon = weaponInput;
                 return true;
             }
             else
             {
-                console.log("O");
                 document.getElementById("O").className = "player-selected";
                 playerOneWeapon = weaponInput;
+                return true;
+            }
+        }
+        else 
+        {
+            return false; // unnsuccesful
+        }
+
+    }
+
+    function setPlayer (playerName)
+    {
+        if (playerName == "Inu")
+        {
+            if (document.getElementById("Karasu").className = "player-selected") // check if other characters are selected and remove status
+            {
+                document.getElementById("Karasu").className = "player-select"; 
+                document.getElementById("Inu").className = "player-selected";
+                playerOneName = playerName;
+                return true;
+            }
+            else
+            {
+                document.getElementById("Inu").className = "player-selected";
+                playerOneName = playerName;
+                return true;
+            }
+
+        }
+        else if (playerName == "Karasu")
+        {   
+            if(document.getElementById("Inu").className == "player-selected")    //check for other character selections
+            {
+                document.getElementById("Inu").className = "player-select"; 
+                document.getElementById("Karasu").className = "player-selected";
+                playerOneName = playerName;
+                return true;
+            }
+            else    // nothing selected
+            {
+                document.getElementById("Karasu").className = "player-selected";
+                playerOneName = playerName;
                 return true;
             }
 
@@ -354,62 +362,15 @@ const gameController = (function ()
 
     }
 
-    function setPlayer (playerName)
-    {
-        if (playerName == "Inu")
-        {
-            if (document.getElementById("Karasu").className = "player-selected") // check if other characters are selected
-            {
-                document.getElementById("Karasu").className = "player-select"; // unselect karasu 
-                document.getElementById("Inu").className = "player-selected";
-                playerOneName = playerName;
-                console.log('playername is :' + playerOneName);
-                return true;
-            }
-            else
-            {
-                document.getElementById("Inu").className = "player-selected";
-                playerOneName = playerName;
-                console.log('playername is :' + playerOneName);
-                return true;
-            }
-
-        }
-        else if (playerName == "Karasu")
-        {   
-            if(document.getElementById("Inu").className == "player-selected")    //check for other character selections
-            {
-                document.getElementById("Inu").className = "player-select"; // de select Inu character
-                document.getElementById("Karasu").className = "player-selected";
-                playerOneName = playerName;
-                console.log('playername is :' + playerOneName);
-                return true;
-            }
-            else    // nothing selected
-            {
-                document.getElementById("Karasu").className = "player-selected";
-                playerOneName = playerName;
-                console.log('playername is :' + playerOneName);
-                return true;
-            }
-
-        }
-        else 
-        {
-            return false; // didnt complete
-        }
-
-    }
-
     const gamePlay = (function () 
     {
-            let playerTurn = "someones turn"; // will initialize with start game
+            let playerTurn = "someones turn";
             let gameBoard = document.getElementById('container'); // create gameBoard for selection
 
             
             let startGame = function () { 
-                playerTurn = playerOne; // Initialize starting turn. 
-                gameMat.playersTurnDisplay (playerTurn); // Initialize display for first player
+                playerTurn = playerOne; 
+                gameMat.playersTurnDisplay (playerTurn);
                 gameBoard.addEventListener("click", playerSelect);
                 return true;
             } 
@@ -431,20 +392,21 @@ const gameController = (function ()
                                 gameMat.playersTurnDisplay(playerOne); // changes display with player swap
                                 return playerTurn = playerOne;
                                 }
-                                else {
-                                } // basecase
+                                else 
+                                {
+                                } 
                             }
-                        else // gameBoard said nope!
+                        else // gameBoard said selection wasnt posible
                         {
                         }
         }
 
         return {startGame, playerSelect}
-    });
-        
-    function refreshPage () //               current call         gameController.gamePlay().endGame();
+    }); // end of gamePlay Module
+
+
+    function refreshPage ()
     {
-    //        window.addEventListener("click", reloadPage);
         location.reload();
     }
 
@@ -452,4 +414,4 @@ const gameController = (function ()
 
     return {setWeapon, setPlayer, createPlayers, letsBegin, gamePlay, refreshPage};
 
-}) (); // game Controller module end 
+}) (); // end of gameController Module
